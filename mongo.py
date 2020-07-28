8ad862672c83a326ec758ac270d93be6f5e0c15d
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2020-07-28 21:46:30 +0000

    Adding mongo.py

---
+++ mongo.py
@@ -0,0 +1,20 @@
+import os
+from pymongo import MongoClient 
+if os.path.exists("env.py"):
+    import env
+
+MONGODB_URI = os.environ.get("MONGO_URI")
+DBS_NAME = "myTestDB"
+COLLECTION_NAME ="myFirstDB"
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+conn = mongo_connect(MONGODB_URI)
+coll = conn[DBS_NAME][COLLECTION_NAME]
+documents = coll.find()
+for doc in documents:
+    print(doc)
